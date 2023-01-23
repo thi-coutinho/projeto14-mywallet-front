464104efa63475ab008f89c4979a13c434c67df2
thi-coutinho <thi.coutinho.santos@gmail.com>
2023-01-23 17:47:16 -0300

    feat: balance on HomePage, some bugfixes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,16 +3,29 @@ import { useEffect, useState } from "react"
 import { ThreeDots } from "react-loader-spinner"
 import { useNavigate } from "react-router-dom"
 import styled from "styled-components"
-import { GREEN, GREY, LIGHTGREY, LIGHTPURPLE, RED, WHITE } from "../constants/colors"
+import { BLACK, GREEN, LIGHTGREY, LIGHTPURPLE, RED, WHITE } from "../constants/colors"
 import { BASE_URL } from "../constants/url"
-// import { useLoading } from "../context/LoadingProvider"
-import { useToken } from "../context/TokenProvider"
+import { useSetToken, useToken } from "../context/TokenProvider"
 
 export default function HomePage() {
     const token = useToken()
+    const setToken = useSetToken()
     const navigate = useNavigate()
     const [entriesList, setEntriesList] = useState(undefined)
+    let balance = undefined;
+
+    if (entriesList?.length > 0) {
+        balance = 0
+        entriesList.filter((d) => d.entryType === "income").forEach(e => {
+            balance += Number(e.value)
+        })
+        entriesList.filter((d) => d.entryType === "expense").forEach(e => {
+            balance -= Number(e.value)
+        })
+    }
+
     useEffect(() => {
+
         const config = {
             headers: {
                 "Authorization": `Bearer ${token.token}`
@@ -23,30 +36,46 @@ export default function HomePage() {
                 setEntriesList(res.data)
             })
             .catch(err => console.log(err.response.data))
-    },[token])
+    }, [token])
     if (!entriesList) {
         return <ThreeDots color={WHITE} />
     }
+
+    function exit() {
+        setToken({})
+        navigate("/")
+    }
     return (
         <>
-            <HelloText>Olá, {`${token.name}`}</HelloText>
+            <HelloText>
+                <span> Olá, {`${token.name}`}</span>
+                <ion-icon onClick={exit} name="exit-outline"></ion-icon>
+            </HelloText>
             <EntriesConteiner>
                 {entriesList.length === 0 && <p>Não há registros de
                     entrada ou saída</p>}
-                {entriesList.map((e) => (
+                <ul>
+                    {entriesList.map((e) => (
 
-                    <Entry key={e._id} entryType={e.entryType}>
-                        <span>{e.date.slice(0, 5)}</span>  {e.description}  <p>{Number(e.value).toFixed(2)}</p>
-                    </Entry>
-                )
-                )}
+                        <Entry key={e._id} entryType={e.entryType}>
+                            <p><span>{e.date.slice(0, 5)}</span>  {e.description}</p>  <p>{Number(e.value).toFixed(2)}</p>
+                        </Entry>
+                    )
+                    )
+                    }
+                </ul>
+                {balance &&
+                    <Balance positive={balance > 0}>
+                        <p>Saldo</p>
+                        <p>{balance.toFixed(2)}</p>
+                    </Balance>}
             </EntriesConteiner>
             <ButtonsConteiner>
-                <AddEntryButton onClick={()=>navigate("/nova-entrada")}>
+                <AddEntryButton onClick={() => navigate("/nova-entrada")}>
                     <ion-icon name="add-circle-outline"></ion-icon>
                     <p>Nova Entrada</p>
                 </AddEntryButton>
-                <AddEntryButton onClick={()=>navigate("/nova-saida")}>
+                <AddEntryButton onClick={() => navigate("/nova-saida")}>
                     <ion-icon name="remove-circle-outline"></ion-icon>
                     <p>Nova Saída</p>
                 </AddEntryButton>
@@ -56,6 +85,8 @@ export default function HomePage() {
 }
 
 const HelloText = styled.div`
+    display:flex;
+    justify-content:space-between;
     margin:0 25px;
     font-family: 'Raleway', Courier, monospace;
     font-weight: 700;
@@ -64,21 +95,25 @@ const HelloText = styled.div`
     color: ${WHITE};
 `
 const EntriesConteiner = styled.div`
+    position:relative;
     margin: 25px;
     height: 446px;
     background-color:${WHITE};
-    padding: 10px;
+    padding: 20px 20px 40px;
     line-height:28px;
     border-radius: 5px;
-    overflow-y:auto;
+    ul {
+        height:390px;
+        overflow-y:auto;
+    }
     p {
-        color: ${GREY}
+        color: ${BLACK}
     }
 `
-const Entry = styled.div`
+const Entry = styled.li`
     display: flex;
     justify-content:space-between;
-    p {
+    p:nth-child(2) {
         color: ${props => props.entryType === "income" ? `${GREEN}` : `${RED}`}
     }
     span {
@@ -109,4 +144,20 @@ const ButtonsConteiner = styled.div`
     margin: 11px 25px;
     display: flex;
     justify-content:space-between;
+`
+
+const Balance = styled.div`
+    font-family: 'Raleway';
+    font-weight: 700;
+    font-size: 17px;
+    line-height: 20px;
+    position:sticky;
+    display:flex;
+    justify-content:space-between;
+    background-color:${WHITE};
+    bottom:0px;
+    height:30px;
+    p:nth-child(2) {
+        color: ${props => props.positive ? `${GREEN}` : `${RED}`}
+    }
 `
